df6c51feb5a0efab4e83b879175aa5257067bcb5
Martin Herweg <martin@martinherweg.de>
2016-11-27 19:59:03 +0100

    complete css task with postcss

--- gulpfile/tasks/compile-css.js
+++ gulpfile/tasks/compile-css.js
@@ -2,8 +2,13 @@ import config from '../../config.json';
 import gulp from 'gulp';
 import path from 'path';
 import gulpLoadPlugins from 'gulp-load-plugins';
-var browserSync = require('browser-sync').create();
+import yargs from 'yargs';
+import browserSync from 'browser-sync';
 import error_handler from '../lib/error-handler';
+import postCSS_config from '../lib/postCSS_config';
+import postCSS_nano from '../lib/postCSS_nano';
+
+const argv = yargs.argv;
 
 const $ = gulpLoadPlugins();
 
@@ -13,12 +18,15 @@ const paths = {
 }
 
 const compileCss = () => {
+  const env = argv.env || 'development'
   return gulp
     .src(paths.src)
-    .pipe($.sourcemaps.init())
+    .pipe($.if(env === 'development', $.sourcemaps.init()))
     .pipe($.sass())
     .on('error', error_handler)
-    .pipe($.sourcemaps.write())
+    .pipe($.postcss(postCSS_config()))
+    .pipe($.if(env === 'development', $.sourcemaps.write('.')))
+    .pipe($.if(env === 'production', $.postcss(postCSS_nano())))
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream({
       match: '**/*.css'
