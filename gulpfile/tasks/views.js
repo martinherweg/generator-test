7a925cc57264695baa5e6efa01ef0a2ddfcc72cd
Martin Herweg <martin@martinherweg.de>
2016-11-27 20:15:00 +0100

    added views task

---
+++ gulpfile/tasks/views.js
@@ -0,0 +1,23 @@
+import config from '../../config.json';
+
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import path from 'path';
+
+const $ = gulpLoadPlugins();
+
+const paths = {
+  src: `${config.src.base}${config.src.views}**/*`,
+  dest: `${config.dist.views}`
+}
+
+const move_views = () => {
+  gulp
+    .src(paths.src)
+    .pipe($.changed(paths.dest))
+    .pipe(gulp.dest(paths.dest))
+}
+
+gulp.task('move:views', move_views);
+
+export default move_views;
