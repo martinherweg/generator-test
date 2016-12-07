4f4d118ebf99ae2d68e443fc148d3686fcfb4409
Martin Herweg <martin@martinherweg.de>
2016-12-07 14:47:19 +0100

    added filed to gitignore

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-// this file gets compiles by browserify
-// please import your partial files here
-// your partials can be in any other folder than 'json', 'my-source' or 'single'
-// these folders are used for other tasks
-// you can change the name of this file in the config.json
-
-
-import 'lazysizes'
-import 'picturefill'
