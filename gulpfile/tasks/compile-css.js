6646967f945aeeffe42ae289965dae473e58d9de
Martin Herweg <martin@martinherweg.de>
2016-12-07 14:45:42 +0100

    added compile css

--- gulpfile/tasks/compile-css.js
+++ gulpfile/tasks/compile-css.js
@@ -8,13 +8,15 @@ import error_handler from '../lib/error-handler';
 import postCSS_config from '../lib/postCSS_config';
 import postCSS_nano from '../lib/postCSS_nano';
 
+import browser_sync_task from './browser-sync';
+
 const argv = yargs.argv;
 
 const $ = gulpLoadPlugins();
 
 const paths = {
   src: path.join(config.src.base, config.src.css, '**/*.scss'),
-  dest: path.join(config.dist.dist, config.dist.css)
+  dest: path.join(config.dist.base, config.dist.css)
 }
 
 const compileCss = () => {
@@ -23,7 +25,17 @@ const compileCss = () => {
     .src(paths.src)
     .pipe($.if(env === 'development', $.sourcemaps.init()))
     .pipe($.sass())
-    .on('error', error_handler)
+    .on('error', function(err) {
+      const error = `
+      <div class="bs-fullscreen" style="position: fixed; top: 0; left: 0; width: 100%; background: rgba(0,0,0,.85); height: 100vh; color: #e8e8e8; text-align: left; white-space: pre;
+  font-family: Menlo, Consolas, monospace; font-size: 13px; padding: 10px; line-height: 1.2;">
+  <pre>${err}</pre> 
+${err.message}
+</div>
+`
+      browserSync.notify(error, 100000);
+      this.emit('end');
+    })
     .pipe($.postcss(postCSS_config()))
     .pipe($.if(env === 'development', $.sourcemaps.write('.')))
     .pipe($.if(env === 'production', $.postcss(postCSS_nano())))
